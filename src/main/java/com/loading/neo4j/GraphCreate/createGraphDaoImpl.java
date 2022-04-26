b31c216610608060b11397360f57130207e9d5f9
lzl <2427320912@qq.com>
2022-04-26 20:25:55 +0800

    update details

---
+++ src/main/java/com/loading/neo4j/GraphCreate/createGraphDaoImpl.java
@@ -0,0 +1,66 @@
+package com.loading.neo4j.GraphCreate;
+
+import com.loading.neo4j.datainteract.criminalContent;
+import com.loading.neo4j.entity.*;
+import com.loading.neo4j.service.GraphService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+
+public class createGraphDaoImpl {
+    @Autowired
+    private GraphService graphService;
+    criminalContent c;
+    Criminal criminalA;
+    public createGraphDaoImpl(criminalContent c){
+        this.c = c;
+        createCriminal();
+        createCrime();
+        createLaw();
+        createCircumstance();
+        createJudgement();
+    }
+    private void createCriminal(){
+        criminalA = new Criminal();
+        String name = c.getCriminalName();
+        criminalA.setNodeName(name);
+        //criminalA.setAge(20);
+        graphService.saveNode(criminalA);
+    }
+    private void createJudgement(){
+        Judgment j = new Judgment(c.getJudgement());
+        graphService.saveNode(j);
+        RelationShip reA = new RelationShip(criminalA,j);
+        reA.setRelationName("punish");
+    }
+    private void createCrime(){
+        List<String> crimeList = c.getCrimeList();
+        for(String s: crimeList){
+            Crime crime = new Crime(s);
+            graphService.saveNode(crime);
+            RelationShip reA = new RelationShip(criminalA,crime);
+            reA.setRelationName("convicted");
+        }
+
+    }
+    private void createLaw(){
+        List<String> lawList = c.getArticles_law();
+        for(String s: lawList){
+            Law law = new Law(s);
+            graphService.saveNode(law);
+            RelationShip reA = new RelationShip(criminalA,law);
+            reA.setRelationName("apply");
+        }
+
+    }
+    private void createCircumstance(){
+        List<String> cList = c.getCircumstance();
+        Circumstance circu = new Circumstance(cList);
+        graphService.saveNode(circu);
+        RelationShip reA = new RelationShip(criminalA,circu);
+        reA.setRelationName("accuse");
+
+    }
+
+}
---
+++ src/main/java/com/loading/neo4j/service/GraphCreateService.java
@@ -0,0 +1,7 @@
+package com.loading.neo4j.service;
+
+import com.loading.neo4j.datainteract.criminalContent;
+
+public class GraphCreateService {
+
+}

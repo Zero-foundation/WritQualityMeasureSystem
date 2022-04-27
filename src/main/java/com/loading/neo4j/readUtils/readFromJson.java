1e2d65d60d0e23e53a4a499e5699751c4084f120
181250214 <181250214@smail.nju.edu.cn>
2022-04-27 20:38:40 +0800

    新建了两个dto，写了readFromJSON

--- src/main/java/com/loading/neo4j/readUtils/readFromJson.java
+++ src/main/java/com/loading/neo4j/readUtils/readFromJson.java
@@ -18,8 +18,8 @@ public class readFromJson {
         String content="";
         String encoding = "UTF-8";
         File file = new File(fileName);
-        Long filelength = file.length();
-        byte[] filecontent = new byte[filelength.intValue()];
+        long filelength = file.length();
+        byte[] filecontent = new byte[(int) filelength];
         try {
             FileInputStream in = new FileInputStream(file);
             in.read(filecontent);
